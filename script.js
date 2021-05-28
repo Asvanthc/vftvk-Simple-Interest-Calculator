124ccc2e0aeb08cf47a0c779f2dba6856995adc8
Asvanth C <83056038+Asvanthc@users.noreply.github.com>
2021-05-28 09:40:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,33 @@
-function rateValue(val){
-	document.getElementById('rateInput').value=val;
+function compute()
+{
+		//Create variables from input
+    let principal = document.getElementById("principal").value;
+    let rate = document.getElementById("rate").value;
+    let years = document.getElementById("years").value;
+    
+    //do math
+    let interest = principal * years * rate / 100;
+    let endYear = new Date().getFullYear() + parseInt(years);
 
+    //compound interest formula
+    let compounded = principal * ((1 + ((rate/100)/12)) ** (12*years))
+		
+    //validate the principal. If negative or 0 alert user and focus on principal
+    if(principal <= 0){
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+        return false;
+    }
+    
+    //display result
+    document.getElementById("result").innerHTML = `If you deposit <mark>${principal}</mark>,<br>at an interest rate of 
+    <mark>${rate}</mark>.<br>You will receive an amount of <mark>${interest.toFixed(2)}</mark>,<br>in the year <mark>${endYear}</mark><br>
+    <br>With monthly compounded interest your balance would instead be <mark>${compounded.toFixed(2)}</mark>`;
 }
 
-/* fonction de calcul de l'intérêt*/
-function calculate(){
-	/* déclaration des variables*/
-amount = document.getElementById('amount').value;
-rateInput= document.getElementById('rateInput').value;
-selectId=document.getElementById('selectId').value;
-resultat=document.getElementById('resultat');
-
-/* calcul du nombre d'année*/
-var today= new Date();
-var annee = today.getFullYear();
-var result1=((annee=+selectId));
-var today2=new Date();
-var annee2 =today2.getFullYear();
-var result2=result1+annee2
-
-/* calul et affichage de l'intérêt*/
-resultat.innerHTML="If you deposit"+"&nbsp;"+(amount)+","+"<br/>"+ "at an interest rate of"+"&nbsp;"+(rateInput)+"."+"<br/>"+
-"You will receive an amount of"+"&nbsp;"+(amount*rateInput*selectId/100)+","+"<br/>"+
-"in the year"+"&nbsp;"+(result2);
+//display rate % by slider
+function rangeReader()
+{
+    let sliderValue = document.getElementById("rate").value;
+    document.getElementById("sliderValue").innerText = `${sliderValue}%`;
 }
